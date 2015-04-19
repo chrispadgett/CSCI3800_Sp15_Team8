8fd2c123b9adb77770be3f02c295c7f18ab1a0b1
chrispadgett <chris.e.padgett@gmail.com>
2015-04-19 17:02:36 -0600

    Updated Location module
    Location module should work now

--- api/controllers/location.js
+++ api/controllers/location.js
@@ -2,6 +2,8 @@
 
 var util = require('util');
 var usergrid = require('usergrid');
+var request = require('request');
+var helper = require('../helpers/helper');
 
 module.exports = {
   locationTest: locationTest
@@ -18,49 +20,117 @@ function locationTest(req, res) {
 	
 	//  Used to test location lookup code easily
 	var ipParam = req.swagger.params.ip.value;
-	var location = getLocation(ipParam);
-	var responseMessage = util.format('Location of ip, %s is %s!', ipParam, location);
-
-	res.json(responseMessage);
+	getLocation(ipParam, function(location) {
+		console.log('Sending Location Test Response');
+		res.json(location);
+	});
 }
 
-function getLocation(ip) {
-	//  This should return a location string
-	//  Be sure to check for the location in the BaaS before doing a lookup
-	//  Also be sure to store the ip and location in the BaaS for future use
-	var location = 'testLocation';
-	return location;
+function getLocation(ip, callback) {
+	// First check if we already have the location in BaaS
+	checkBaaSForLocation(ip, function(location) {
+		//  If it doesn't exist, look it up and add it
+		if(!location)
+		{
+			lookupLocation(ip, function(location) {
+				if (location)
+				{
+					storeLocationInBaaS(location, function(loc) {
+						callback(loc)
+					});
+				}
+			});
+		}
+		else
+		{
+			callback(location);
+		}
+	});
 }
 
-function checkBaaSForLocation(ip) {
+function checkBaaSForLocation(ip, callback) {
 	//  This function should check the BaaS for the location before doing full lookup
-	//  I added the BaaS info to this function
-	var inBaaS = 0;
-	var location = 'testLocation';
+	console.log(util.format('Checking BaaS for ip %s', ip));
 	var dataClient = new usergrid.client({
 		orgName:'csci3800_sp15_team8',
 		appName:'Sandbox'
 	});
 	
 	var options = {
-		endpoint:'locations'
-		//  A query will be needed here
+		endpoint:'locations',
+		qs:{ql:"select * where ip='" + ip + "'"}
 	};
 	
 	dataClient.request(options, function (error, response) {
-		//  If in BaaS, set a location and change 
+		if (error)
+		{
+			console.log(util.format('There was an error checking the BaaS for ip %s', ip));
+			console.log(error);
+			callback(0);
+		}
+		else
+		{
+			if(response.entities.length > 0)
+			{
+				console.log('Location exists in the BaaS');
+				console.log(response.entities[0]);
+				callback(response.entities[0]);
+			}
+			else
+			{
+				console.log(util.format('Location does not exist in BaaS'));
+				console.log(response);
+				callback(0);
+			}
+		}
 	});
-	
-	if(inBaaS)
-	{
-		return location;
-	}
-	else
-	{
-		return 0;
-	}
 }
 
-function storeLocationInBaaS(ip, location) {
-	// This funciton should store the ip and location in the BaaS for future use
+function storeLocationInBaaS(locationJSON, callback) {
+	console.log(util.format('Storing ip %s in the BaaS for future use', locationJSON.ip));
+	var dataClient = new usergrid.client({
+		orgName:'csci3800_sp15_team8',
+		appName:'Sandbox'
+	});
+
+	var optionsPOST = {
+		method:'POST',
+		endpoint:'locations',
+		qs:{ql:"select * where ip='" + locationJSON.ip + "'"},
+		body:locationJSON
+	};
+	
+	dataClient.request(optionsPOST, function (error, response) {
+		if (error)
+		{
+			console.log('Error in POST to BaaS');
+			console.log(error);
+			callback(locationJSON);
+		} 
+		else 
+		{
+			console.log('POST to BaaS successful');
+			console.log(response);
+			callback(locationJSON);
+		}
+	});
+}
+
+function lookupLocation(ip, callback) {
+	console.log(util.format('Looking up location of %s', ip));
+	var url = 'http://www.telize.com/geoip/' + ip;
+	
+	request({url: url, json: true}, function (error, response, body) {
+		if (!error && response.statusCode == 200)
+		{
+			console.log(util.format('Lookup for %s successful', ip));
+			console.log(body);
+			callback(body);
+		}
+		else
+		{
+			console.log(error);
+			callback(0);
+		}
+	})
 }
--- api/helpers/helper.js
+++ api/helpers/helper.js
@@ -1,10 +1,43 @@
 'use strict';
 
+var usergrid = require('usergrid');
+
 module.exports = {
-  passwordCheck: passwordCheck
+  passwordCheck: passwordCheck,
+  isEmptyObject: isEmptyObject
 };
 
 function passwordCheck(username, password, cb) {
-  var passwordOk = (username === 'chris' && password === 'chrispass');
-  cb(null, passwordOk);
+	// We will replace this with a lookup to the BaaS
+	var passwordOk = (username === 'test' && password === 'test');
+	cb(null, passwordOk);
+	
+	/*
+	var inBaaS = 0;
+	var location = 'testLocation';
+	var dataClient = new usergrid.client({
+		orgName:'csci3800_sp15_team8',
+		appName:'Sandbox'
+	});
+	
+	var options = {
+		endpoint:'paymentusers'
+		//  Need to add a query to check credentials when other code is working
+	};
+	
+	dataClient.request(options, function (error, response) {
+		//  check response and act accordingly 
+	});
+	*/
+}
+
+function isEmptyObject(obj) {
+	for (var key in obj)
+	{
+		if (Object.prototype.hasOwnProperty.call(obj, key))
+		{
+		  return false;
+		}
+	}
+	return true;
 }
