d4de6987c9d163db5c03c3723152a23b021f5d74
chrispadgett <chris.e.padgett@gmail.com>
2015-05-02 15:47:12 -0600

    Added Location Compare, updated basic auth
    Added location comparison against user accepted locations which are
    stored in the BaaS
    Added url to test location comparison
    Updated basic auth to compare credentials against users in the BaaS

--- api/controllers/location.js
+++ api/controllers/location.js
@@ -6,7 +6,8 @@ var request = require('request');
 var helper = require('../helpers/helper');
 
 module.exports = {
-  locationTest: locationTest
+  locationTest: locationTest,
+  compareUserLocationTest: compareUserLocationTest
 };
 
 /*
@@ -23,6 +24,28 @@ function locationTest(req, res) {
 	getLocation(ipParam, function(location) {
 		console.log('Sending Location Test Response');
 		res.json(location);
+	});
+}
+
+function compareUserLocationTest(req, res) {
+	console.log('Compare User to Location Test Request Received');
+	
+	//  Used to test if a user is authorized to use a location code easily
+	var ipParam = req.swagger.params.ip.value;
+	var paymentUsernameParam = req.swagger.params.paymentusername.value;
+	getLocation(ipParam, function(location) {
+		compareLocation(paymentUsernameParam, location, function(authorized) {
+			if(authorized)
+			{
+				console.log('The user is authorized to use this location');
+				res.json('The user is authorized to use this location');
+			}
+			else
+			{
+				console.log('The user is NOT authorized to use this location');
+				res.json('The user is NOT authorized to use this location');
+			}
+		});
 	});
 }
 
@@ -133,4 +156,66 @@ function lookupLocation(ip, callback) {
 			callback(0);
 		}
 	})
+}
+
+function compareLocation(paymentUsername, locationResponse, callback) {
+	console.log(util.format('Comparing %s against accepted locations', paymentUsername));
+	
+	// Quick check to see if the location has a city
+	// not all ips have a city as documented in the API we are calling against
+	if(!locationResponse.city)
+	{
+		console.log('The ip location does not have a city.');
+		callback(false);
+	}
+	else
+	{
+		var dataClient = new usergrid.client({
+			orgName:'csci3800_sp15_team8',
+			appName:'Sandbox'
+		});
+		
+		var options = {
+			endpoint:'paymentusers',
+			qs:{ql:"select * where username='" + paymentUsername + "'"}
+		};
+		
+		dataClient.request(options, function (error, response) {
+			if (error)
+			{
+				console.log(util.format('There was an error checking the BaaS for paymentUser %s', paymentUsername));
+				console.log(error);
+				callback(false);
+			}
+			else
+			{
+				if(response.entities.length == 0)
+				{
+					console.log(util.format('paymentUser %s was not found in the BaaS.', paymentUsername));
+					callback(false);
+				}
+				else
+				{
+					checkAllLocations(locationResponse.city, response.entities[0].acceptedLocations, function(paymentLocationAccepted) {
+						callback(paymentLocationAccepted);
+					});
+				}
+			}
+		});
+	}
+}
+
+function checkAllLocations(acceptableLocation, locationArray, callback) {
+	var locationFound = false;
+	for(var i = 0; i < locationArray.length; ++i)
+	{
+		console.log(util.format('Checking accepted location: %s', locationArray[i]));
+		if(locationArray[i] === acceptableLocation)
+		{
+			console.log('A location has been accepted');
+			locationFound = true;
+		}
+	}
+	
+	callback(locationFound);
 }
--- api/helpers/helper.js
+++ api/helpers/helper.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var usergrid = require('usergrid');
+var util = require('util');
 
 module.exports = {
   passwordCheck: passwordCheck,
@@ -8,27 +9,44 @@ module.exports = {
 };
 
 function passwordCheck(username, password, cb) {
-	// We will replace this with a lookup to the BaaS
-	var passwordOk = (username === 'test' && password === 'test');
-	cb(null, passwordOk);
+	console.log('Checking basic auth credentials');
 	
-	/*
-	var inBaaS = 0;
-	var location = 'testLocation';
 	var dataClient = new usergrid.client({
 		orgName:'csci3800_sp15_team8',
 		appName:'Sandbox'
 	});
 	
 	var options = {
-		endpoint:'paymentusers'
-		//  Need to add a query to check credentials when other code is working
+		endpoint:'paymentusers',
+		qs:{ql:"select * where username='" + username + "'"}
 	};
 	
 	dataClient.request(options, function (error, response) {
-		//  check response and act accordingly 
+		if (error)
+		{
+			console.log(util.format('There was an error checking the BaaS for paymentUser %s', username));
+			console.log(error);
+			cb(null, false);
+		}
+		else
+		{
+			if(response.entities.length == 0)
+			{
+				console.log(util.format('paymentUser %s was not found in the BaaS.', username));
+				cb(null, false);
+			}
+			else if(response.entities[0].password === password)
+			{
+				console.log('paymentUser had the correct password');
+				cb(null, true);
+			}
+			else
+			{
+				console.log(util.format('payumentUser did not have the correct password'));
+				cb(null, false);
+			}
+		}
 	});
-	*/
 }
 
 function isEmptyObject(obj) {
